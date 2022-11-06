27b0da0c3b7f92a040ba64954a6c7d4dfda87fd8
Parkduksung <duksung1234@naver.com>
2022-11-06 21:43:50 +0900

    [feature] buildSrc 추가

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,36 @@
+object Versions {
+
+    //app level
+    const val gradle = "7.0.2"
+    const val kotlin = "1.5.31"
+
+
+    //libs
+    val coreKtx = "1.7.0"
+    val appCompat = "1.3.1"
+    val material = "1.4.0"
+    val constraintlayout = "2.1.3"
+
+    //test
+    val junit = "4.13.2"
+    val extJunit = "1.1.3"
+    val espresso = "3.4.0"
+
+    //mockito
+    val mockitoCore = "3.3.3"
+    val mockitoAll = "1.10.19"
+    val mockitoInline = "3.3.3"
+
+    //jetpack
+    val viewmodel = "2.4.0"
+    val hilt = "2.44"
+    val room = "2.4.2"
+    val fragment = "1.4.1"
+    val activity = "1.4.0"
+    val hilt_navigation = "1.0.0"
+
+    //network
+    val retrofit = "2.9.0"
+    val gson = "2.9.0"
+    val scalars = "2.6.0"
+}
